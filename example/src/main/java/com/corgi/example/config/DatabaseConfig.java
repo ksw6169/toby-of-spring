c11c121c89c96d0f93451ccd894484ace12d8b58
ksw6169 <ksw6169@naver.com>
2021-03-01 23:58:00 +0900

    [과제#11] DataSource Bean 설정 변경

--- example/src/main/java/com/corgi/example/config/DatabaseConfig.java
+++ example/src/main/java/com/corgi/example/config/DatabaseConfig.java
@@ -1,5 +1,7 @@
 package com.corgi.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,6 +15,19 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
@@ -33,5 +48,15 @@ public class DatabaseConfig {
     @Bean
     public DataSourceTransactionManager transactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(driverClassName)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
     }
 }
